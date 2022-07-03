c9c65cb9773cc3b25031c681f3da9e493f597409
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2022-07-04 01:39:20 +0530

    added Home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Home from './pages/homepage/Home';
 import Login from './pages/login/Login';
+import Cart from './pages/cart/Cart';
+import Products from './components/Products';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/products' element={<Products />} />
       </Routes>
       <Footer />
     </>
--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -1,6 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+const imageUrls = {
+  hero: 'https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/katariy/BAU/OP_Dec/D24582943_IN_WL_Category_Page_1500x400.gif',
+  mobile:
+    'https://images.pexels.com/photos/6608247/pexels-photo-6608247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  electronics:
+    'https://images.pexels.com/photos/955405/pexels-photo-955405.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+};
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className='main-container'>
+      <header className='main-header-item'>
+        <img
+          className='hero-img'
+          src='https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/katariy/BAU/OP_Dec/D24582943_IN_WL_Category_Page_1500x400.gif'
+          alt='hero'
+        />
+        {/* <h2 className='hero-heading'>
+            Get top deals on Mobiles and Electronics.
+          </h2> */}
+      </header>
+
+      <div className='main-item'>
+        <Link to='/products' className='section'>
+          <img
+            src='https://images.pexels.com/photos/404280/pexels-photo-404280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            alt='mobile'
+            className='section-photo'
+          />
+          <h2 className='section-desc'>Top selling Mobiles</h2>
+          <p className='section-offer'>upto 20% off</p>
+        </Link>
+
+        <Link to='/products' className='section'>
+          <img
+            src='https://images.pexels.com/photos/955405/pexels-photo-955405.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            alt='electronics'
+            className='section-photo'
+          />
+          <h2 className='section-desc'>Best Laptops deals</h2>
+          <p className='section-offer'>with extended warranty offer</p>
+        </Link>
+      </div>
+    </main>
+  );
 };
+
 export default Home;
