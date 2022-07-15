aad776143e9aca477463c41a007cb26c62b4d251
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2022-07-15 21:26:22 +0530

    fix: cart items

---
+++ src/components/CartCard.jsx
@@ -0,0 +1,56 @@
+import PropTypes from 'prop-types';
+import '../styles/Card.css';
+import { useDispatch } from 'react-redux';
+
+import { removeFromCartAction } from '../redux/features/cartSlice';
+
+const CartCard = props => {
+  const {
+    productName,
+    features,
+    offer,
+    description,
+    price,
+    mrp,
+    discount,
+    rating,
+    imageUrl,
+    productId,
+  } = props.data;
+
+  const dispatch = useDispatch();
+  return (
+    <div className='card-container'>
+      <div className='card-img'>
+        <img src={imageUrl} alt='product' className='card-photo'></img>
+      </div>
+      <h2 className='card-main-heading'>{productName}</h2>
+      <p className='card-desc'>{description}</p>
+      <div className='rating'>Rating {rating}</div>
+      <p>
+        <span>{price}</span>
+        <span className='strike-price'>{mrp}</span>
+        <span className='discount'>{discount}</span>
+      </p>
+      <p className='features'>{features}</p>
+      <p className='discount'>{offer}</p>
+      <span
+        className='material-symbols-outlined delete-icon'
+        onClick={() => {
+          dispatch(removeFromCartAction(productId));
+        }}
+      >
+        delete
+      </span>
+    </div>
+  );
+};
+
+CartCard.propTypes = {
+  data: PropTypes.object,
+};
+
+CartCard.defaultProps = {
+  data: {},
+};
+export default CartCard;
--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,24 +1,44 @@
 import { Link } from 'react-router-dom';
 import emptyImage from '../../images/emptyImage.webp';
 import './Cart.css';
+import { useSelector } from 'react-redux';
+import { cartItemsSelector } from '../../redux/selectors';
+import { products } from '../../mockData';
+import CartCard from '../../components/CartCard';
 
 const Cart = () => {
+  const cartItems = useSelector(cartItemsSelector);
   return (
     <div className='cart-wrapper'>
       <div className='cart-header'>
-        <h2 className='heading'>My Cart</h2>
+        <h2 className='heading'>My Cart ({cartItems.length})</h2>
         <div className='cart-details'></div>
       </div>
-      <div className='empty-cart-body'>
-        <div className='cart-img'>
-          <img className='cart-empty-img' src={emptyImage} alt='Empty Cart' />
+      {cartItems.length ? (
+        <div className='non-empty-cart-body'>
+          <div className='cart-items'>
+            {products
+              .filter(item => cartItems.includes(item.productId))
+              .map(item => (
+                <CartCard key={item.productId} data={item} />
+              ))}
+          </div>
+          <div className='price-details'>
+            <h2>price details</h2>hello
+          </div>
         </div>
-        <p className='cart-desc'>Your cart is empty!</p>
-        <p className='cart-msg'>Add items to it now.</p>
-        <Link to='/' className='shop-now-btn'>
-          Shop Now
-        </Link>
-      </div>
+      ) : (
+        <div className='empty-cart-body'>
+          <div className='cart-img'>
+            <img className='cart-empty-img' src={emptyImage} alt='Empty Cart' />
+          </div>
+          <p className='cart-desc'>Your cart is empty!</p>
+          <p className='cart-msg'>Add items to it now.</p>
+          <Link to='/' className='shop-now-btn'>
+            Shop Now
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
